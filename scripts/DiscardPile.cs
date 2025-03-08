accea6c0710d7a95ee78b99e30c6ac6dc4b68e81
marcs-sus <lilianemarcos2018@gmail.com>
2025-03-08 18:28:20 -0300

    Discard pile

---
+++ scripts/DiscardPile.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using ScoundrelGame;
+
+public partial class DiscardPile : Control
+{
+	private List<Card> discardPile = new List<Card>();
+	public int DiscardedCards { get => discardPile.Count; }
+
+	public void DiscardCard(Card card)
+	{
+		if (card.GetParent() != null)
+		{
+			card.GetParent().RemoveChild(card);
+			card.slotPosition = Position;
+		}
+
+		discardPile.Add(card);
+
+		AddChild(card); // Optional visualization effect
+	}
+
+	public void ClearDiscardPile()
+	{
+		foreach (Card card in discardPile)
+		{
+			card.QueueFree();
+		}
+		discardPile.Clear();
+	}
+}
